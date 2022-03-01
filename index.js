46afd448f9cba7bf8f796ccf32b10c21886dedee
Hentiofyle <92610008+Hentiofyle@users.noreply.github.com>
2022-03-01 23:40:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+let num = document.getElementById('num').value
+let metersEl = document.getElementById('meters')
+let feetEl = document.getElementById('feet')
+let litersEl = document.getElementById('liters')
+let gallonsEl = document.getElementById('gallons')
+let kilosEl = document.getElementById('kilos')
+let poundsEl = document.getElementById('pounds')
+
+num.textContent = num 
+
+
+
+
+function conversion(){
+    num = document.getElementById('num').value
+     let resultMeters =  num  * 3.280
+    let resultFeet =  num  * 0.340
+
+    let roundedFeet = resultFeet.toFixed(3)
+    let roundedMeters = resultMeters.toFixed(3)
+
+    metersEl.textContent = num +" meters = " +roundedMeters +" feet"
+    feetEl.textContent = num +" feet = " +roundedFeet + " meters"
+
+    let resultLiters = num * 0.264
+    let resultGallons = num *3.785
+
+     let roundedLiters = resultLiters.toFixed(3)
+    let roundedGallons = resultGallons.toFixed(3)
+    
+    litersEl.textContent = num +" liters = " +roundedLiters +" gallons"
+    gallonsEl.textContent =  num +" gallons = " +roundedGallons +" liters"
+    
+    let resultKilos = num * 2.204
+    let resultPounds = num / 2.204
+
+     let roundedKilos = resultKilos.toFixed(3)
+    let roundedPounds = resultPounds.toFixed(3)
+    
+    kilosEl.textContent = num +" kilos = " +roundedKilos +" pounds"
+    poundsEl.textContent =  num +" pounds = " +roundedPounds +" kilos"  
+}
+
+conversion()
+
+
+//theme selector 
+
+const themeSelector = document.querySelector("#themes")
+const themeLink = document.querySelector(".theme")
+
+function setTheme() {
+	let theme = themeSelector.value
+	themeLink.setAttribute("href", "theme-" + theme + ".css")
+}
+
+themeSelector.addEventListener("change",function(){
+	setTheme()
+})
+
+setTheme()
